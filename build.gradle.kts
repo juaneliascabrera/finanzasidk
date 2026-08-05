8a0af6d7b84934c0cb2302bf9f37321ea06e1ec0
juaneliascabrera <jecabrera@dc.uba.ar>
2026-08-05 01:42:49 +0000

    build: crea proyecto Gradle mínimo con módulo core

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    kotlin("jvm") version "2.0.21" apply false
+}
+
+allprojects {
+    group = "com.finanzas"
+    version = "0.1.0-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ core/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    kotlin("jvm")
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
